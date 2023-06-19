fd8b7812f356f8105e01b4be936073edbfb9bf6e
Arksyd96 <aghiles.kebaili.1998@gmail.com>
2023-06-19 14:13:00 +0200

    [ignore]

--- modules/autoencoders.py
+++ modules/autoencoders.py
@@ -139,9 +139,6 @@ class VariationalAutoencoder(nn.Module):
     ) -> None:
         super().__init__()
 
-        device_idx = torch.cuda.current_device()
-        self.local_device = torch.device(f"cuda:{device_idx}" if torch.cuda.is_available() else "cpu")
-
         self.input_shape = np.array(input_shape)
         self.z_channels = z_channels
         in_channels = out_channels = self.input_shape[0]
@@ -160,8 +157,8 @@ class VariationalAutoencoder(nn.Module):
 
         # define a N(0, I) distribution
         self.normal = torch.distributions.MultivariateNormal(
-            loc=torch.zeros(self.latent_dim).to(self.local_device),
-            covariance_matrix=torch.eye(self.latent_dim).to(self.local_device),
+            loc=torch.zeros(self.latent_dim),
+            covariance_matrix=torch.eye(self.latent_dim),
         )
 
     def forward(self, x, pemb=None):
@@ -331,7 +328,7 @@ class HamiltonianAutoencoder(VariationalAutoencoder, pl.LightningModule):
             self, input_shape, z_channels, pemb_dim, num_channels, channels_mult, num_res_blocks, attn
         )
         
-        self.positional_encoder = TimePositionalEmbedding(pemb_dim, T, local_device=self.local_device)
+        self.positional_encoder = TimePositionalEmbedding(pemb_dim, T, local_device=self.device)
 
         self.vae_forward = super().forward
         self.n_lf = n_lf
@@ -346,6 +343,12 @@ class HamiltonianAutoencoder(VariationalAutoencoder, pl.LightningModule):
         )
 
         self.regularization = LPIPSWithDiscriminator(**kwargs['loss'])
+
+        # define a N(0, I) distribution [Overriding because of the GPU]
+        self.normal = torch.distributions.MultivariateNormal(
+            loc=torch.zeros(self.latent_dim).to(self.device),
+            covariance_matrix=torch.eye(self.latent_dim).to(self.device),
+        )
 
         self.save_hyperparameters()
         self.automatic_optimization = False
